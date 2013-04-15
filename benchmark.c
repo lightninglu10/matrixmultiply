743a88d6622966f19b2cd0072c391d7e193ecb76
lightninglu10 <lightning.lu7@gmail.com>
2013-04-14 19:46:07 -0700

    final version, 11.5 gflops

--- benchmark.c
+++ benchmark.c
@@ -34,7 +34,7 @@ int main( int argc, char **argv )
 	for( int n = 40; n <  41; n = n+1+n/3 )
 	{
   /* Try different m */
-  //for( int m = 32; m <  100; m = m+1+m/3 )
+//for( int m = 32; m <  100; m = m+1+m/3 )
 for( int m = 48; m <  49; m = m+1+m/3 )
   {
     /* Allocate and fill 2 random matrices A, C */
--- sgemm-small.c
+++ sgemm-small.c
@@ -5,7 +5,7 @@
 
 
 void sgemm( int m, int n, int d, float *A, float *C ) {
-    __m128 vect, ATmatrix, ATmatrix1, ATmatrix2, ATmatrix3, ATmatrix4, ATmatrix5, ATmatrix6, ATmatrix7, Cmatrix;
+    __m128 vect, ATmatrix, ATmatrix1, ATmatrix2, ATmatrix3, ATmatrix4, ATmatrix5, ATmatrix6, ATmatrix7, ATmatrix8, ATmatrix9, Cmatrix, AMatrix, AMatrix1, AMatrix2, AMatrix3, AMatrix4;
 
     int k, i, j;
 
@@ -16,96 +16,259 @@ void sgemm( int m, int n, int d, float *A, float *C ) {
 			    ATmatrix1 =  _mm_load1_ps(A + ((j + 1) * (n + 1) + (k) * (n)));
 			    ATmatrix2 = _mm_load1_ps(A + ((j + 2) * (n + 1) + (k) * (n)));
 			    ATmatrix3 =  _mm_load1_ps(A + ((j + 3) * (n + 1) + (k) * (n)));
-			    ATmatrix4 = _mm_load1_ps(A + ((j+4) * (n + 1) + (k) * (n)));
+			    ATmatrix4 = _mm_load1_ps(A + ((j + 4) * (n + 1) + (k) * (n)));
 			    ATmatrix5 =  _mm_load1_ps(A + ((j + 5) * (n + 1) + (k) * (n)));
 			    ATmatrix6 = _mm_load1_ps(A + ((j + 6) * (n + 1) + (k) * (n)));
 			    ATmatrix7 =  _mm_load1_ps(A + ((j + 7) * (n + 1) + (k) * (n)));
-			    for(i = 0; i < n/8 * 8; i += 8) {
-			    	float *p = A + i + k * n;
+/*			    ATmatrix8 = _mm_load1_ps(A + ((j + 8) * (n + 1) + (k) * (n)));
+			    ATmatrix9 =  _mm_load1_ps(A + ((j + 9) * (n + 1) + (k) * (n)));*/
+			    for(i = 0; i < n/20 * 20; i += 20) {
+
+			    	AMatrix = _mm_loadu_ps(A + i + k * n);
+			    	AMatrix1 = _mm_loadu_ps((A + i + k * n) + 4);
+			    	AMatrix2 = _mm_loadu_ps((A + i + k * n) + 8);
+			    	AMatrix3 = _mm_loadu_ps((A + i + k * n) + 12);
+			    	AMatrix4 = _mm_loadu_ps((A + i + k * n) + 16);
+
 
 					Cmatrix = _mm_loadu_ps(C + i + j * n);
-					vect = _mm_mul_ps(_mm_loadu_ps(p), ATmatrix);
+					vect = _mm_mul_ps(AMatrix, ATmatrix);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
-					_mm_storeu_ps(C + (i + j * n), Cmatrix);
+					_mm_storeu_ps(C + i + j * n, Cmatrix);
 
 					Cmatrix = _mm_loadu_ps((C + i + j * n) + 4);
-					vect = _mm_mul_ps(_mm_loadu_ps((p) + 4), ATmatrix);
+					vect = _mm_mul_ps(AMatrix1, ATmatrix);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps((C + i + j * n) + 4, Cmatrix);
 
+					Cmatrix = _mm_loadu_ps((C + i + j * n) + 8);
+					vect = _mm_mul_ps(AMatrix2, ATmatrix);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + j * n) + 8, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + j * n) + 12);
+					vect = _mm_mul_ps(AMatrix3, ATmatrix);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + j * n) + 12, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + j * n) + 16);
+					vect = _mm_mul_ps(AMatrix4, ATmatrix);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + j * n) + 16, Cmatrix);
+
+
+					//j = 1
+
 					Cmatrix = _mm_loadu_ps(C + i + (j + 1) * n);
-					vect = _mm_mul_ps(_mm_loadu_ps(p), ATmatrix1);
+					vect = _mm_mul_ps(AMatrix, ATmatrix1);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps(C + (i + (j + 1) * n), Cmatrix);
 
 					Cmatrix = _mm_loadu_ps((C + i + (j + 1) * n) + 4);
-					vect = _mm_mul_ps(_mm_loadu_ps((p) + 4), ATmatrix1);
+					vect = _mm_mul_ps(AMatrix1, ATmatrix1);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps((C + i + (j + 1) * n) + 4, Cmatrix);
 
+					Cmatrix = _mm_loadu_ps((C + i + (j + 1) * n) + 8);
+					vect = _mm_mul_ps(AMatrix2, ATmatrix1);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 1) * n) + 8, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 1) * n) + 12);
+					vect = _mm_mul_ps(AMatrix3, ATmatrix1);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 1) * n) + 12, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 1) * n) + 16);
+					vect = _mm_mul_ps(AMatrix4, ATmatrix1);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 1) * n) + 16, Cmatrix);
+
+					//j = 2
+
 					Cmatrix = _mm_loadu_ps(C + i + (j + 2) * n);
-					vect = _mm_mul_ps(_mm_loadu_ps(p), ATmatrix2);
+					vect = _mm_mul_ps(AMatrix, ATmatrix2);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps(C + (i + (j + 2) * n), Cmatrix);
 
 					Cmatrix = _mm_loadu_ps((C + i + (j + 2) * n) + 4);
-					vect = _mm_mul_ps(_mm_loadu_ps((p) + 4), ATmatrix2);
+					vect = _mm_mul_ps(AMatrix1, ATmatrix2);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps((C + i + (j + 2) * n) + 4, Cmatrix);
 
+					Cmatrix = _mm_loadu_ps((C + i + (j + 2) * n) + 8);
+					vect = _mm_mul_ps(AMatrix2, ATmatrix2);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 2) * n) + 8, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 2) * n) + 12);
+					vect = _mm_mul_ps(AMatrix3, ATmatrix2);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 2) * n) + 12, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 2) * n) + 16);
+					vect = _mm_mul_ps(AMatrix4, ATmatrix2);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 2) * n) + 16, Cmatrix);
+
+					//j = 3
+
 					Cmatrix = _mm_loadu_ps(C + i + (j + 3) * n);
-					vect = _mm_mul_ps(_mm_loadu_ps(p), ATmatrix3);
+					vect = _mm_mul_ps(AMatrix, ATmatrix3);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps(C + (i + (j + 3) * n), Cmatrix);
 
 					Cmatrix = _mm_loadu_ps((C + i + (j + 3) * n) + 4);
-					vect = _mm_mul_ps(_mm_loadu_ps((p) + 4), ATmatrix3);
+					vect = _mm_mul_ps(AMatrix1, ATmatrix3);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps((C + i + (j + 3) * n) + 4, Cmatrix);
 
-					//4-8
+					Cmatrix = _mm_loadu_ps((C + i + (j + 3) * n) + 8);
+					vect = _mm_mul_ps(AMatrix2, ATmatrix3);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 3) * n) + 8, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 3) * n) + 12);
+					vect = _mm_mul_ps(AMatrix3, ATmatrix3);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 3) * n) + 12, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 3) * n) + 16);
+					vect = _mm_mul_ps(AMatrix4, ATmatrix3);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 3) * n) + 16, Cmatrix);
+
+					//j = 4
 
 					Cmatrix = _mm_loadu_ps(C + i + (j + 4) * n);
-					vect = _mm_mul_ps(_mm_loadu_ps(p), ATmatrix4);
+					vect = _mm_mul_ps(AMatrix, ATmatrix4);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps(C + (i + (j + 4) * n), Cmatrix);
 
 					Cmatrix = _mm_loadu_ps((C + i + (j + 4) * n) + 4);
-					vect = _mm_mul_ps(_mm_loadu_ps((p) + 4), ATmatrix4);
+					vect = _mm_mul_ps(AMatrix1, ATmatrix4);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps((C + i + (j + 4) * n) + 4, Cmatrix);
 
-					//5
+					Cmatrix = _mm_loadu_ps((C + i + (j + 4) * n) + 8);
+					vect = _mm_mul_ps(AMatrix2, ATmatrix4);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 4) * n) + 8, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 4) * n) + 12);
+					vect = _mm_mul_ps(AMatrix3, ATmatrix4);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 4) * n) + 12, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 4) * n) + 16);
+					vect = _mm_mul_ps(AMatrix4, ATmatrix4);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 4) * n) + 16, Cmatrix);
+
+					//j = 5
 
 					Cmatrix = _mm_loadu_ps(C + i + (j + 5) * n);
-					vect = _mm_mul_ps(_mm_loadu_ps(p), ATmatrix5);
+					vect = _mm_mul_ps(AMatrix, ATmatrix5);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps(C + (i + (j + 5) * n), Cmatrix);
 
 					Cmatrix = _mm_loadu_ps((C + i + (j + 5) * n) + 4);
-					vect = _mm_mul_ps(_mm_loadu_ps((p) + 4), ATmatrix5);
+					vect = _mm_mul_ps(AMatrix1, ATmatrix5);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps((C + i + (j + 5) * n) + 4, Cmatrix);
 
+					Cmatrix = _mm_loadu_ps((C + i + (j + 5) * n) + 8);
+					vect = _mm_mul_ps(AMatrix2, ATmatrix5);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 5) * n) + 8, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 5) * n) + 12);
+					vect = _mm_mul_ps(AMatrix3, ATmatrix5);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 5) * n) + 12, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 5) * n) + 16);
+					vect = _mm_mul_ps(AMatrix4, ATmatrix5);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 5) * n) + 16, Cmatrix);
+
+					//j = 6
+
 					Cmatrix = _mm_loadu_ps(C + i + (j + 6) * n);
-					vect = _mm_mul_ps(_mm_loadu_ps(p), ATmatrix6);
+					vect = _mm_mul_ps(AMatrix, ATmatrix6);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps(C + (i + (j + 6) * n), Cmatrix);
 
 					Cmatrix = _mm_loadu_ps((C + i + (j + 6) * n) + 4);
-					vect = _mm_mul_ps(_mm_loadu_ps((p) + 4), ATmatrix6);
+					vect = _mm_mul_ps(AMatrix1, ATmatrix6);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps((C + i + (j + 6) * n) + 4, Cmatrix);
 
+					Cmatrix = _mm_loadu_ps((C + i + (j + 6) * n) + 8);
+					vect = _mm_mul_ps(AMatrix2, ATmatrix6);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 6) * n) + 8, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 6) * n) + 12);
+					vect = _mm_mul_ps(AMatrix3, ATmatrix6);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 6) * n) + 12, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 6) * n) + 16);
+					vect = _mm_mul_ps(AMatrix4, ATmatrix6);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 6) * n) + 16, Cmatrix);
+
+					//j = 7
+
 					Cmatrix = _mm_loadu_ps(C + i + (j + 7) * n);
-					vect = _mm_mul_ps(_mm_loadu_ps(p), ATmatrix7);
+					vect = _mm_mul_ps(AMatrix, ATmatrix7);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps(C + (i + (j + 7) * n), Cmatrix);
 
 					Cmatrix = _mm_loadu_ps((C + i + (j + 7) * n) + 4);
-					vect = _mm_mul_ps(_mm_loadu_ps((p) + 4), ATmatrix7);
+					vect = _mm_mul_ps(AMatrix1, ATmatrix7);
 					Cmatrix = _mm_add_ps(Cmatrix, vect);
 					_mm_storeu_ps((C + i + (j + 7) * n) + 4, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 7) * n) + 8);
+					vect = _mm_mul_ps(AMatrix2, ATmatrix7);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 7) * n) + 8, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 7) * n) + 12);
+					vect = _mm_mul_ps(AMatrix3, ATmatrix7);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 7) * n) + 12, Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 7) * n) + 16);
+					vect = _mm_mul_ps(AMatrix4, ATmatrix7);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 7) * n) + 16, Cmatrix);
+
+/*					//j = 8
+
+					Cmatrix = _mm_loadu_ps(C + i + (j + 8) * n);
+					vect = _mm_mul_ps(_mm_loadu_ps(A + i + k * n), ATmatrix8);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps(C + (i + (j + 8) * n), Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 8) * n) + 4);
+					vect = _mm_mul_ps(_mm_loadu_ps((A + i + k * n) + 4), ATmatrix8);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 8) * n) + 4, Cmatrix);
+
+					//j = 9
+
+					Cmatrix = _mm_loadu_ps(C + i + (j + 9) * n);
+					vect = _mm_mul_ps(_mm_loadu_ps(A + i + k * n), ATmatrix9);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps(C + (i + (j + 9) * n), Cmatrix);
+
+					Cmatrix = _mm_loadu_ps((C + i + (j + 9) * n) + 4);
+					vect = _mm_mul_ps(_mm_loadu_ps((A + i + k * n) + 4), ATmatrix9);
+					Cmatrix = _mm_add_ps(Cmatrix, vect);
+					_mm_storeu_ps((C + i + (j + 9) * n) + 4, Cmatrix);*/
 
 			    }
 			}
